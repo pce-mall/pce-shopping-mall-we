0bbb0ea537eac45e91dea9d6315921349ded2059
pce-mall <pceshoppingmall@gmail.com>
2025-09-07 17:29:23 +0100

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB31kOw965UN-2_Gi4pNjOBtVCIDuh3Ef0",
+  authDomain: "pce-shopping-mall.firebaseapp.com",
+  projectId: "pce-shopping-mall",
+  storageBucket: "pce-shopping-mall.firebasestorage.app",
+  messagingSenderId: "10570005342",
+  appId: "1:10570005342:web:c30283e51d78a3b879adcf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
