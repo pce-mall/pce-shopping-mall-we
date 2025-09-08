fbb0c5b0ac8e7033fb827ed7fe9ee1735d680161
pce-mall <pceshoppingmall@gmail.com>
2025-09-08 05:22:00 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB31kOw965UN-2_Gi4pNjOBtVCIDuh3Ef0",
-  authDomain: "pce-shopping-mall.firebaseapp.com",
-  projectId: "pce-shopping-mall",
-  storageBucket: "pce-shopping-mall.firebasestorage.app",
-  messagingSenderId: "10570005342",
-  appId: "1:10570005342:web:c30283e51d78a3b879adcf"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export Firebase services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
