9829bf2af07f81a570f7e3c531e507129d436fc0
pce-mall <pceshoppingmall@gmail.com>
2025-09-09 21:38:43 +0100

    Update Account.js

--- src/Account.js
+++ src/Account.js
@@ -23,11 +23,11 @@ import { initializeApp } from "firebase/app";
 
 // TODO: Replace with your Firebase project config
 const firebaseConfig = {
-  apiKey:AIzaSyB31kOw965UN-2_Gi4pNjOBtVCIDuh3Ef0 ,
-  authDomain: pce-shopping-mall.firebaseapp.com,
-  projectId: pce-shopping-mall,
-  storageBucket: pce-shopping-mall.firebasestorage.app,
-  messagingSenderId:10570005342,
+  apiKey:"AIzaSyB31kOw965UN-2_Gi4pNjOBtVCIDuh3Ef0",
+  authDomain: "pce-shopping-mall.firebaseapp.com",
+  projectId: "pce-shopping-mall",
+  storageBucket: "pce-shopping-mall.firebasestorage.app",
+  messagingSenderId:"10570005342",
   appId: "1:10570005342:web:c30283e51d78a3b879adcf"
 };
 
