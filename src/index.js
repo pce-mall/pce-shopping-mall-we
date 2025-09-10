eb9001eb64a2823dae104779651a85cbca4d40dd
pce-mall <pceshoppingmall@gmail.com>
2025-09-10 07:38:16 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+// src/index.js
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import App from "./App";
-import "./index.css";  // optional
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(<App />);
