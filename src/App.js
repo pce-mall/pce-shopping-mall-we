1b169a28ae42a995b0ef717657af117b3102b516
pce-mall <pceshoppingmall@gmail.com>
2025-09-10 07:51:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React from "react";
-import Account from "./Account";
-
+// src/App.js
 export default function App() {
-  return <Account />;
-    }
+  return (
+    <div style={{padding: 24, fontFamily: "system-ui"}}>
+      <h1>✅ React is rendering</h1>
+      <p>If you can see this after deploy, the issue is inside your app code, not the setup.</p>
+    </div>
+  );
+}
