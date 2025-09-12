c54cc16cf9bcf2cd7c241652ab17f80cbf131c58
pce-mall <pceshoppingmall@gmail.com>
2025-09-12 06:54:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ export default function App(){
   return (
     <>
       <Landing />
-      {/* seller/user area below the storefront */}
       <div style={{margin:"32px auto",maxWidth:1100,padding:"0 16px"}}>
         <Account />
       </div>
