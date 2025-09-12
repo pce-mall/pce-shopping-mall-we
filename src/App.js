56a69ca8fa5c8576dc985c792ba4b1b540b9d5b2
pce-mall <pceshoppingmall@gmail.com>
2025-09-12 06:51:27 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Landing from "./Landing";
 import Account from "./Account";
 
-export default function App() {
+export default function App(){
   return (
-    <div style={{ minHeight: "100vh", background: "#f7f8fa" }}>
-      <Account />
-    </div>
+    <>
+      <Landing />
+      {/* seller/user area below the storefront */}
+      <div style={{margin:"32px auto",maxWidth:1100,padding:"0 16px"}}>
+        <Account />
+      </div>
+    </>
   );
 }
