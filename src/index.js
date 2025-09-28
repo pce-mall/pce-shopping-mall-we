78d7374988046be755d0c0badbc18b87bdd84034
pce-mall <pceshoppingmall@gmail.com>
2025-09-29 00:12:05 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // optional, only if you have styles
+import "./index.css";  // ✅ make sure this file exists in /src
 
-// Render the App into the root div (from public/index.html)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
