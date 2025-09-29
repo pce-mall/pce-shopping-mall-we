d0a2c6188b4f9f355ac2f6e41104cc8d595af4b3
pce-mall <pceshoppingmall@gmail.com>
2025-09-29 07:14:28 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from "./App";
-import "./index.css";  // ✅ make sure this file exists in /src
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
