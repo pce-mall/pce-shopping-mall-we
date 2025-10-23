6380a2e25f1c1e67648654b4afe9fd5cbb675eae
pce-mall <pceshoppingmall@gmail.com>
2025-10-23 11:05:43 +0100

    Delete src directory

--- src/components/CategoryCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function CategoryCard({icon, title, text}){
-  return (
-    <div className="card">
-      <div style={{fontSize:"24px"}}>{icon}</div>
-      <h3>{title}</h3>
-      <p>{text}</p>
-    </div>
-  );
-}
