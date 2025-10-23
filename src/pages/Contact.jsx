8a7ae70a7f4977384387da59195d29afd5c19c1a
pce-mall <pceshoppingmall@gmail.com>
2025-10-23 12:59:56 +0100

    Create Contact.jsx

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <section style={{ padding: '20px' }}>
+      <h2>Contact Us</h2>
+      <p>Email: <a href="mailto:pceshoppingmall@gmail.com">pceshoppingmall@gmail.com</a></p>
+      <p>WhatsApp: <a href="https://wa.me/2347089724573" target="_blank" rel="noreferrer">Chat with us</a></p>
+    </section>
+  )
+}
