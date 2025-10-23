3704a904d3fbaa7615da9a9831b3deec5a7943f8
pce-mall <pceshoppingmall@gmail.com>
2025-10-23 11:51:56 +0100

    Add About component for PCE Shopping Mall

---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <section style={{ padding: '20px' }}>
+      <h2>About PCE Shopping Mall</h2>
+      <p>PCE Shopping Mall connects buyers and sellers globally, delivering trusted products with fast and secure payments.</p>
+    </section>
+  )
+}
