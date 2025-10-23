5bc596c006b2e36cd8e031cc6da10fbff6d2bbb8
pce-mall <pceshoppingmall@gmail.com>
2025-10-23 20:01:20 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './styles.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./styles.css";
+import { CartProvider } from "./context/CartContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
