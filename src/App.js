2b62d6e645f151c788339ee970a8f3c4472df635
pce-mall <pceshoppingmall@gmail.com>
2025-10-23 09:31:54 +0100

    Initialize App component with routing and layout

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Categories from "./pages/Categories";
+import Contact from "./pages/Contact";
+
+export default function App() {
+  return (
+    <>
+      <NavBar />
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
+}
