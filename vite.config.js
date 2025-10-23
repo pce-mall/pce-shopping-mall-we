6d4d9098a95765e7c405357cda0cda9fcec9777d
pce-mall <pceshoppingmall@gmail.com>
2025-10-23 10:55:06 +0100

    Refactor Vite config for build and server options

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: '.',              // project root
-  publicDir: 'public',    // folder where index.html is located
   plugins: [react()],
   build: {
-    outDir: 'dist',       // Netlify will publish this folder
-    rollupOptions: {
-      external: []        // don’t externalize anything
-    }
-  }
+    outDir: 'dist',
+    rollupOptions: { input: 'index.html' }
+  },
+  server: { open: true }
 })
