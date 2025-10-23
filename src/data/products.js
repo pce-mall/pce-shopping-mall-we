fc8a4768ff65e9af16bff54a291677dcc7208b55
pce-mall <pceshoppingmall@gmail.com>
2025-10-23 14:06:28 +0100

    Create products.js

---
+++ src/data/products.js
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: 1,
+    name: "Smart Watch",
+    price: 25000,
+    image: "https://images.unsplash.com/photo-1519744346363-63f00a92e3b1?auto=format&w=500",
+  },
+  {
+    id: 2,
+    name: "Wireless Headphones",
+    price: 18000,
+    image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?auto=format&w=500",
+  },
+  {
+    id: 3,
+    name: "Casual Sneakers",
+    price: 15000,
+    image: "https://images.unsplash.com/photo-1606813907291-9dfb9ee24b1c?auto=format&w=500",
+  },
+  {
+    id: 4,
+    name: "Laptop Bag",
+    price: 12000,
+    image: "https://images.unsplash.com/photo-1610484826967-09c65ef0ed07?auto=format&w=500",
+  },
+];
